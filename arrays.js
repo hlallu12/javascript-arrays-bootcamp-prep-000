08349cb388919bdf158004c5537749805d62016f
hansalallu@gmail.com <hansalallu@gmail.com>
2018-08-26 09:44:32 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -17,7 +17,7 @@ function addElementToEndOfArray(array, element){
 function accessElementInArray(array, index){
   return array[index]
 }
-function destructivelyRemoveElementFromBeginningOfArray(array){
+function destructivelyRemoveElementFromBeginningOfArray(array, element){
   array.shift(element)
   return array
 }
