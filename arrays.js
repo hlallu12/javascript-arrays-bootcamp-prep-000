b5be747e1b852386ce5e344ed381f2b380862277
hansalallu@gmail.com <hansalallu@gmail.com>
2018-08-26 10:24:15 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -33,5 +33,5 @@ function destructivelyRemoveElementFromEndOfArray(array, element){
   return array
 }
 function removeElementFromEndOfArray(array, element){
-  return array.slice(.length - element)
+  return array.slice(array.length - element)
 }
